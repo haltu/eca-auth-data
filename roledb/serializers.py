446681c39ced208486601a3c1cf41da405e7ec86
Ilkka Hakkari <ilkka@haltu.fi>
2014-10-20 18:25:22 +0300

    Moved also serializers.py to correct location waiting for merge

--- roledb/serializers.py
+++ roledb/serializers.py
@@ -5,10 +5,11 @@ from roledb.models import User
 
 class UserSerializer(serializers.ModelSerializer):
   roles = serializers.SerializerMethodField('role_data')
+  attributes = serializers.SerializerMethodField('attribute_data')
 
   class Meta:
     model = User
-    fields = ('username', 'roles')
+    fields = ('username', 'roles', 'attributes')
 
   def role_data(self, obj):
     data = []
@@ -20,4 +21,12 @@ class UserSerializer(serializers.ModelSerializer):
       data.append(d)
     return data
 
+  def attribute_data(self, obj):
+    data = []
+    for a in obj.attributes.all():
+      d = {}
+      d['name'] = a.attribute.name
+      d['value'] = a.value
+      data.append(d)
+    return data
 
